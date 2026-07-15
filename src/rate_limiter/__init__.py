3148e6c024fff47bd7476a4655c2cd5c723dd8b4
Mallem Sofiane <melsopv@gmail.com>
2026-07-15 14:03:14 +0200

    make rate limiter installable pkg, ignore build artifacts

--- src/rate_limiter/__init__.py
+++ src/rate_limiter/__init__.py
@@ -1,11 +1,15 @@
-from rate_limiter.algorithms.fixed_window import FixedWindow
-from rate_limiter.algorithms.sliding_window import SlidingWindow
-from rate_limiter.algorithms.token_bucket import TokenBucket
+from rate_limiter.algorithms import (
+    FixedWindow,
+    SlidingWindow,
+    TokenBucket,
+)
 from rate_limiter.decorators import rate_limit
+from rate_limiter.schemas import RateLimitResult
 
 __all__ = [
     "FixedWindow",
     "SlidingWindow",
     "TokenBucket",
+    "RateLimitResult",
     "rate_limit",
 ]
